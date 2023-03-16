d318b6b641bb44c5bd42fecb101427e95c5448b3
CYRIELLE THOMAS <cyriellethomas.pro@gmail.com>
2023-03-16 11:27:54 +0100

    Atelier node.js Chalk

---
+++ script.js
@@ -0,0 +1,9 @@
+import chalk from 'chalk';
+
+console.log(chalk.yellow('Marta'));
+console.log(chalk.green('Coucou'));
+console.log(chalk.blue('Hello'));
+
+const color =chalk.hex("203057")
+console.log(color('Cyrielle'));
+
